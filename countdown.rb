ef14670977fed13579efbe20a9546715d2c44be9
David Moore <hey@iamdavidmoore.com>
2018-04-17 23:30:33 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,10 @@ def countdown(int)
   while int > 0 do
     puts "#{int} SECOND(S)!"
     int -= 1
-    sleep(1)
   end
   "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  sleep(seconds)
 end
